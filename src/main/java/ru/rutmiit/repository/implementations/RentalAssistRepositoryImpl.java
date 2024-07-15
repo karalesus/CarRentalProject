647d9af1bec478845613a4d9cb0d4eb3f4e7fc8c
karalesus <karalesus@gmail.com>
2024-07-15 23:46:08 +0300

    add RentalAssistRepositoryImpl

--- src/main/java/ru/rutmiit/repository/implementations/RentalAssistRepositoryImpl.java
+++ src/main/java/ru/rutmiit/repository/implementations/RentalAssistRepositoryImpl.java
@@ -1,9 +1,13 @@
 package ru.rutmiit.repository.implementations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import ru.rutmiit.domain.RentalAssist;
 import ru.rutmiit.repository.BaseRepository;
 
+@Repository
 public class RentalAssistRepositoryImpl extends BaseRepository<RentalAssist, Integer> {
+
     public RentalAssistRepositoryImpl(Class<RentalAssist> rentalAssistClass) {
         super(rentalAssistClass);
     }
