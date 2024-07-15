ef79f1c880aabd49506c0c60a12f70712401d248
karalesus <karalesus@gmail.com>
2024-07-16 01:42:50 +0300

    add updating status in PaymentRepositoryImpl

--- src/main/java/ru/rutmiit/repository/implementations/PaymentRepositoryImpl.java
+++ src/main/java/ru/rutmiit/repository/implementations/PaymentRepositoryImpl.java
@@ -3,7 +3,9 @@ package ru.rutmiit.repository.implementations;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.rutmiit.domain.Payment;
+import ru.rutmiit.domain.enums.PaymentStatus;
 import ru.rutmiit.repository.BaseRepository;
 import ru.rutmiit.repository.PaymentRepository;
 
@@ -24,5 +26,15 @@ public class PaymentRepositoryImpl extends BaseRepository<Payment, Integer> impl
         return entityManager.createQuery("SELECT p from Payment p JOIN p.client c where c.id = :clientId", Payment.class)
                 .setParameter("clientId", clientId)
                 .getResultList();
+    }
+
+    @Override
+    @Transactional
+    public Payment updateStatus(Integer id, PaymentStatus paymentStatus) {
+         entityManager.createQuery("UPDATE Payment p SET p.paymentStatus = :paymentStatus WHERE p.id = :id ", Payment.class)
+                .setParameter("paymentStatus", paymentStatus)
+                .setParameter("id", id)
+                .executeUpdate();
+                return entityManager.find(Payment.class, id);
     }
 }
