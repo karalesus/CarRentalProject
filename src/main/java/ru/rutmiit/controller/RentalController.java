5de2271d3c57c3577d4ff02dcac210610bf6d878
karalesus <karalesus@gmail.com>
2024-07-16 21:49:10 +0300

    add controllers for car and rental

---
+++ src/main/java/ru/rutmiit/controller/RentalController.java
@@ -0,0 +1,33 @@
+package ru.rutmiit.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.rutmiit.dto.CarDTO;
+import ru.rutmiit.dto.RentalDTO;
+import ru.rutmiit.service.implementations.CarServiceImpl;
+import ru.rutmiit.service.implementations.RentalServiceImpl;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rentals")
+public class RentalController {
+
+    @Autowired
+    private RentalServiceImpl rentalServiceImpl;
+
+    @Autowired
+    private CarServiceImpl carServiceImpl;
+
+    @GetMapping("/available-cars")
+    public List<CarDTO> getAvailableCars(@RequestParam("startDate") LocalDate startDate, @RequestParam("finishDate") LocalDate finishDate) {
+        return carServiceImpl.getAvailableCarsByDate(startDate, finishDate);
+    }
+
+    @PostMapping("/rent")
+    public RentalDTO rentCar(@RequestBody RentalDTO rentalDTO) {
+        return rentalServiceImpl.rentCar(rentalDTO);
+    }
+}
+
